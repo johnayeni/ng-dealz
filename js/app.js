5a9a446c802e8b04af86b95921157e48699368a3
John Ayeni <johnayeni10.aj@gmail.com>
2019-04-07 17:31:33 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const generateDealCard = (deal) => {
+  return `
+    <div class="shop-card">
+      <div class="shop-card-image">
+        <img
+          class="card-img"
+          src="${deal.image}"
+          width="100"
+          height="100"
+        />
+      </div>
+      <div class="shop-card-details">
+        <h5 class="text-primary">${deal.name}</h5>
+        <h4 class="text-accent">${deal.price.replace(/₦/g, '&nbsp;₦')}</h3>
+        <h1 class="text-secondary">${deal.discount}</h1>
+        <p>${deal.time_left}</p>
+      </div>
+    </div>
+  `;
+};
+
+const displayDeals = (deals, container) => {
+  const containerToFill = document.querySelector(`#${container}`);
+  deals.map((deal) => {
+    containerToFill.innerHTML = `
+      ${containerToFill.innerHTML}
+      ${generateDealCard(deal)}
+    `;
+  });
+};
+
+const fetchKongaDeals = async () => {
+  const fetchUrl = 'http://ng-dealz-webscrapper.herokuapp.com/get-deals';
+  const response = await fetch(fetchUrl);
+  const deals = await response.json();
+  displayDeals(deals, 'konga-deals');
+  const loader = document.querySelector('.loader');
+  loader.style.display = 'none';
+};
+
+setInterval(() => {
+  fetchKongaDeals();
+}, 15000);
